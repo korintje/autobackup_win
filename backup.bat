97e741e4c5117d9efd330052202e3a2d3a977571
Takuro Hosomi <30950088+korintje@users.noreply.github.com>
2023-06-22 14:42:57 +0900

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,18 +1,12 @@
 @echo off
 
 set CONFIG=config.ini
-REM set SERVER_CONFIG=server_config.ini
 cd /d %~dp0
 
 REM Load config.ini
 for /f "tokens=1,* delims==" %%a in (%CONFIG%) do (
     set %%a=%%b
 )
-
-REM Load server_config.ini
-REM for /f "tokens=1,* delims==" %%a in (%SERVER_CONFIG%) do (
-REM     set %%a=%%b
-REM )
 
 REM Connect to NAS
 net use "%DST_DIR%" /user:%SERVER_USERNAME% %SERVER_PASSWORD%
